d2cef8a45d4dec8ad8672668490841265f7cb799
沈长青 <changqing.shen@tenclass.com>
2023-06-07 15:13:50 +0800

    fix one deploy one pod

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,38 @@
+use std::collections::BTreeMap;
+
 use futures::StreamExt;
 use k8s_openapi::api::core::v1::Pod;
 
 use kube::{
     api::{Api, AttachParams, AttachedProcess, ListParams},
+    core::ObjectList,
     Client,
 };
+
+#[derive(Debug, Clone)]
+struct DeploymentPod {
+    deploy: String,
+    ns: String,
+    pod: String,
+}
+#[derive(Debug, PartialEq, Eq, Clone)]
+struct DeploymentNs {
+    deploy: String,
+    ns: String,
+}
+
+impl PartialOrd for DeploymentNs {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+impl Ord for DeploymentNs {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.ns
+            .cmp(&other.ns)
+            .then_with(|| self.deploy.cmp(&other.deploy))
+    }
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -18,31 +46,33 @@ async fn main() -> anyhow::Result<()> {
     let pod_list = pods_all_ns.list(&list_params).await?;
 
     let cmd = vec!["cat", "/etc/os-release"];
-    let container_name = "app";
 
-    for pod in pod_list {
-        if get_running_pod_with_container(&pod, container_name) {
-            let name = pod.metadata.name.as_ref().unwrap();
-            let ns = pod.metadata.namespace.as_ref().unwrap();
-            let kube_pod: Api<Pod> = Api::namespaced(client.clone(), ns.as_str());
+    let dp_list = get_deploy_one_pod(pod_list);
+    for dp in dp_list {
+        let name = dp.pod;
+        let ns = dp.ns;
+        let kube_pod: Api<Pod> = Api::namespaced(client.clone(), ns.as_str());
 
-            let attached = kube_pod
-                .exec(name, cmd.clone(), &AttachParams::default().stderr(false))
-                .await?;
+        let attached = kube_pod
+            .exec(
+                &name,
+                cmd.clone(),
+                &AttachParams::default().stderr(false).container("app"),
+            )
+            .await?;
 
-            let output = get_output(attached).await;
+        let output = get_output(attached).await;
 
-            let lines = output.lines();
-            for line in lines {
-                if line.starts_with("ID=") {
-                    println!(
-                        "ns={} pod={} get os={}",
-                        ns,
-                        name,
-                        line.strip_prefix("ID=").unwrap()
-                    );
-                    break;
-                }
+        let lines = output.lines();
+        for line in lines {
+            if line.starts_with("ID=") {
+                println!(
+                    "ns={} pod={} get os={}",
+                    ns,
+                    name,
+                    line.strip_prefix("ID=").unwrap()
+                );
+                break;
             }
         }
     }
@@ -74,4 +104,57 @@ fn get_running_pod_with_container(p: &Pod, name: &str) -> bool {
         }
     }
     false
+}
+
+// generate by gpt4
+fn get_deploy_one_pod(pod_list: ObjectList<Pod>) -> Vec<DeploymentPod> {
+    let mut dp_vec: Vec<DeploymentPod> = Vec::new();
+    let container_name = "app";
+
+    for pod in pod_list {
+        if let Some(owner_references) = pod.metadata.owner_references.as_ref() {
+            if let Some(owner_reference) = owner_references.get(0) {
+                if owner_reference.kind == "ReplicaSet" {
+                    if let (Some(hash), Some(generate_name)) = (
+                        pod.metadata
+                            .labels
+                            .as_ref()
+                            .and_then(|labels| labels.get("pod-template-hash")),
+                        pod.metadata.generate_name.as_ref(),
+                    ) {
+                        let dp_name = generate_name
+                            .split(&format!("-{}-", hash))
+                            .nth(0)
+                            .unwrap()
+                            .to_string();
+
+                        if get_running_pod_with_container(&pod, container_name) {
+                            let dp = DeploymentPod {
+                                pod: pod.metadata.name.clone().unwrap(),
+                                deploy: dp_name,
+                                ns: pod.metadata.namespace.clone().unwrap(),
+                            };
+                            dp_vec.push(dp)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    let mut dp_map = BTreeMap::new();
+    for dp_value in dp_vec {
+        let dn = DeploymentNs {
+            deploy: dp_value.deploy.clone(),
+            ns: dp_value.ns.clone(),
+        };
+
+        dp_map.insert(dn.clone(), dp_value);
+    }
+
+    let mut result: Vec<DeploymentPod> = Vec::new();
+    for (_, v) in dp_map {
+        result.push(v.clone());
+    }
+    result
 }
